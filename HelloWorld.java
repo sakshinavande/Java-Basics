ef9bdb94c0753e4576ff0bc6ea23c3bce60f7f68
Sakshi Navande <sakshinavande93@gmail.com>
2026-07-05 09:18:27 +0530

    Add HelloWorld Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is my first Java program.");
+        System.out.println("Excited to begin my Java Developer journey!");
+    }
+}
