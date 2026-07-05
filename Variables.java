7ff2437b9af18f3f6484bcc11b3b4d83c7edbe2e
sakshinavande <sakshinavande93@gmail.com>
2026-07-05 15:37:56 +0530

    Add Variables.java demonstrating Java primitive data types

---
+++ Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Sakshi";
+        int age = 19;
+        double cgpa = 8.75;
+        float salary = 35000.55f;
+        char grade = 'A';
+        boolean passed = true;
+        long mobile = 755921100099446L;
+        short year = 2026;
+        byte semester = 5;
+
+       System.out.println("Name : "+ name);
+        System.out.println("Age : "+ age);
+        System.out.println("CGPA : "+ cgpa);
+        System.out.println("Salary : "+ salary);
+        System.out.println("Grade : "+ grade);
+        System.out.println("Passed : "+ passed);
+        System.out.println("Mobile : "+ mobile);
+        System.out.println("Year : "+ year);
+        System.out.println("Semester : "+ semester);
+    }
+}
