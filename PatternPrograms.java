919715b7c93b9cd2a82371e77003dfe9d8359eae
Sakshi Navande <sakshinavande93@gmail.com>
2026-07-13 16:31:36 +0530

    Add files via upload

---
+++ PatternPrograms.java
@@ -0,0 +1,31 @@
+public class PatternPrograms {
+    public static void main(String[] args) {
+
+        // Pattern 1
+        System.out.println("Pattern 1:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 2
+        System.out.println("\nPattern 2:");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 3
+        System.out.println("\nPattern 3:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
